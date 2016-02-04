cab7f295dfe7a3fbb706fffc509506c476de3715
Shuai Lin <linshuai2012@gmail.com>
2016-02-04 21:42:36 +0800

    Do not use Chinese apt mirror by default

--- base-provision.sh
+++ base-provision.sh
@@ -9,9 +9,10 @@ update-rc.d -f puppet remove
 service chef-client stop || true
 update-rc.d -f cheif-client remove || true
 
-# Use 163 apt mirrors
-cp /etc/apt/sources.list /etc/apt/sources.list.bak
-cat <<EOF >/tmp/sources.list
+if [[ $USE_CN_MIRROR == "true" ]]; then
+    # Use 163 apt mirrors
+    cp /etc/apt/sources.list /etc/apt/sources.list.bak
+    cat <<EOF >/tmp/sources.list
 deb http://mirrors.163.com/ubuntu/ trusty main restricted universe multiverse
 deb http://mirrors.163.com/ubuntu/ trusty-security main restricted universe multiverse
 deb http://mirrors.163.com/ubuntu/ trusty-updates main restricted universe multiverse
@@ -23,8 +24,9 @@ deb-src http://mirrors.163.com/ubuntu/ trusty-updates main restricted universe m
 deb-src http://mirrors.163.com/ubuntu/ trusty-proposed main restricted universe multiverse
 deb-src http://mirrors.163.com/ubuntu/ trusty-backports main restricted universe multiverse
 EOF
-mv /tmp/sources.list /etc/apt/
-rm -f /etc/apt/sources.list.d/puppetlabs.list
+    mv /tmp/sources.list /etc/apt/
+fi
 
+rm -f /etc/apt/sources.list.d/puppetlabs.list
 
 apt-get update -q
