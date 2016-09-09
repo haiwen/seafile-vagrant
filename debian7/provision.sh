ea96b349ee4738195bac4dc509e2a43a02087566
Shuai Lin <linshuai2012@gmail.com>
2016-09-09 11:53:28 +0800

    Changes for debian7.

--- debian7/provision.sh
+++ debian7/provision.sh
@@ -2,9 +2,6 @@
 
 set -e -x
 
-apt-get update -q
-apt-get install -y tmux htop vim software-properties-common
-
 cat >/home/vagrant/.bashrc<<'EOF'
 if [[ $- == *i* ]]; then
     PS1="\[$(tput bold)\]\[$(tput setaf 2)\][\u@\h \w]\\$ \[$(tput sgr0)\]"
@@ -12,3 +9,12 @@ fi
 export CDPATH=.:/vagrant/src/:/vagrant/data:/vagrant
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
 EOF
+
+apt-get update -q
+apt-get install -y tmux htop vim software-properties-common
+
+# http://forums.debian.net/viewtopic.php?p=404227
+# https://wiki.debian.org/Gnome
+# apt-get install -y --no-install-recommends gnome-core
+aptitude -q --without-recommends -o APT::Install-Recommends=no -y install ~t^desktop$ ~t^gnome-desktop$
+
